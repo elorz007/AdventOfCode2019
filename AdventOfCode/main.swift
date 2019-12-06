bf1d020d557421e39ae1e8e8ee83913c8302300b
Mikel Elorz <mikel.elorz@virtual-solution.com>
2019-12-06 14:20:42 +0100

    Initial Commit

---
+++ AdventOfCode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode
+//
+//  Created by Mikel Elorz on 06.12.19.
+//  Copyright © 2019 Mikel Elorz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
