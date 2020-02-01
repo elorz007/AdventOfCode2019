7a393fdae1977d72e9c65e2462e10a4940761d7e
Mikel Elorz <mikel.elorz@virtual-solution.com>
2020-02-01 11:02:15 +0100

    Basic structure day 9

---
+++ AdventOfCode/Day9.swift
@@ -0,0 +1,15 @@
+//
+//  Day9.swift
+//  AdventOfCode
+//
+//  Created by Mikel Elorz on 01.02.20.
+//  Copyright © 2020 Mikel Elorz. All rights reserved.
+//
+
+import Cocoa
+
+class Day9: NSObject {
+    func input() -> String {
+        try! String(contentsOfFile: "./Day9.txt").trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
